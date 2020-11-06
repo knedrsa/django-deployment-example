a7632ab84781ba4377ccd9841b475053b630edb0
Vamsi Namala <rj.shekar20c@gmail.com>
2020-11-06 19:37:43 +0530

    initial commit

---
+++ blogfive/articles/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from articles import models
+
+class CreateArticle(forms.ModelForm):
+    class Meta:
+        model = models.Article
+        fields = ['title','slug','body','thumb']
---
+++ blogfive/blogfive/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    #return HttpResponse('Hello World')
+    return render(request,'homepage.html')
